8d438b589aa821070987c35bbec64a89f0f2cce8
Shiv Patel <shiv.yuvasoft@gmail.com>
2017-02-14 15:54:50 +0530

    Change deployment branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -46,7 +46,7 @@ set :current_path, '/home/ubuntu/Work/current'
 set :shared_path, '/home/ubuntu/Work/shared'
 
 set :environment , 'production'
-set :branch , 'production'
+set :branch , 'staging'
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
